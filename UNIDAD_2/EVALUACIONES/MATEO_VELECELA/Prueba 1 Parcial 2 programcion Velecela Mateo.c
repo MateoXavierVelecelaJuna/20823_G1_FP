5a98d935033c0aeaa595bee205f0d480c390c559
MateoXavierVelecelaJuna <mxvelecela@espe.edu.ec>
2025-06-04 08:27:40 -0500

    Add files via upload

---
+++ UNIDAD_2/EVALUACIONES/MATEO_VELECELA/Prueba 1 Parcial 2 programcion Velecela Mateo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(){
+  float vec[10];
+  int i,n;
+  float norma=0,
+
+  printf("Ingresar 10 numeros reales:");
+  scanf("%d", &n)
+
+
+for (i = 0; i < 10; i++) {
+        printf("Ingresa el componente para la posicion %d: ", i);
+    }
+
+    for (i = 0; i < 10; i++) {
+       norma = vec[i] * vec[i];
+    }
+
+    printf("Los valores del vector son:\n");
+    for (i = 0; i < 10; i++) {
+        printf("vector[%d] = %.2f\n", i, vec[i]); //dos decimales
+    }
+
+
+
+
+
+ return 0
+
+
+ }
+
