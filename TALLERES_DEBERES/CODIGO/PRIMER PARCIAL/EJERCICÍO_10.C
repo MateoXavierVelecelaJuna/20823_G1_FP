f2e13af6d851aa50bed3ed495ffba75fb5a4f17f
Alexander Sebastian Alvear Montaluisa <asalvear@espe.edu.ec>
2025-07-03 22:25:09 -0500

    Update EJERCICÍO_10.C

--- TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICÍO_10.C
+++ TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICÍO_10.C
@@ -3,7 +3,7 @@
 int main() {
 // Declaración
 
-printf("Ejercicio 10: Regresar a ejercicio 9\n");
+printf("Ejercicio 10: Regresar al ejercicio 9\n");
      printf("\nPresione Enter para regresar al menú...");
     return 0;
 }
