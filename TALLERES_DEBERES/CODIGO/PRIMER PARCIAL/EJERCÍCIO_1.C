454b16c6313eb3617da61bab299ba52385fbc2c4
Alexander Sebastian Alvear Montaluisa <asalvear@espe.edu.ec>
2025-07-03 22:24:39 -0500

    Update EJERCÍCIO_1.C

--- TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCÍCIO_1.C
+++ TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCÍCIO_1.C
@@ -4,7 +4,7 @@ int main()
 {
 float a, b, res; 
     printf("Ejercicio 1: Media de dos valores\n");
-    printf("Ingrese el Primer Numero: ");
+    printf("Ingrese el Primer Número: ");
     scanf("%f", &a);
 
     printf("Ingrese el Segundo Número: ");
