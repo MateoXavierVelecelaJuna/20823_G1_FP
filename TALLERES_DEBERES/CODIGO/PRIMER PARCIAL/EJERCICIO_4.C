8bf0735c9fcf5fb66f85d871dab8d71941b6dd52
Alexander Sebastian Alvear Montaluisa <asalvear@espe.edu.ec>
2025-07-16 07:12:22 -0500

    Update EJERCICIO_4.C

--- TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_4.C
+++ TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_4.C
@@ -7,9 +7,9 @@ float z;
     printf("Ingresa un numero real: ");
     scanf("%f", &z);
     if (z>0 && z<=10){
-        printf("El número SI pertenece al intervalo (0,10]");
+        printf("El numero SI pertenece al intervalo (0,10]");
     } else{
-        printf("El número NO pertenece al intervalo (0,10]");
+        printf("El numero NO pertenece al intervalo (0,10]");
     }
     return 0;
 }
