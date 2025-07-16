1504f032d1c52f08a18093b9774a7b9a6ac5cd6a
Alexander Sebastian Alvear Montaluisa <asalvear@espe.edu.ec>
2025-07-16 07:10:25 -0500

    Update EJERCICIO_18.C

--- TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_18.C
+++ TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_18.C
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
- printf("Ejercicio 18: Mostrar los números pares del 1 al 10\n");
+ printf("Ejercicio 18: Mostrar los numeros pares del 1 al 10\n");
 
     for (int i = 1; i <= 10; i++) {
         if (i % 2 == 0) {
