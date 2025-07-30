ee157796fd32775ffe935698b9ba613d9501220b
Alexander Sebastian Alvear Montaluisa <asalvear@espe.edu.ec>
2025-07-29 21:58:14 -0500

    Update EJERCICIO_17.C

--- TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_17.C
+++ TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_17.C
@@ -5,11 +5,11 @@ int c;
         printf("Ejercicio 17: Pedir numeros hasta ingresar uno negativo\n");
 
     do {
-        printf("Ingrese un número (negativo para terminar): ");
+        printf("Ingrese un numero (negativo para terminar): ");
         scanf("%d", &c);
 
         if (c >= 0) {
-            printf("Número ingresado: %d\n", c);
+            printf("Numero ingresado: %d\n", c);
         }
 
     } while (c >= 0);
