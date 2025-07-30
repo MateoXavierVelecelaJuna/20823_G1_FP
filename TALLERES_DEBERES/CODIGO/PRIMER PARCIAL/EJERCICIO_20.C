7f54b880cfe80df67eafc8af830c102dd4cab84c
Alexander Sebastian Alvear Montaluisa <asalvear@espe.edu.ec>
2025-07-29 21:59:06 -0500

    Update EJERCICIO_20.C

--- TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_20.C
+++ TALLERES_DEBERES/CODIGO/PRIMER PARCIAL/EJERCICIO_20.C
@@ -7,7 +7,7 @@ int main() {
     printf("Ejercicio 20: Calcular el promedio de 5 números ingresados\n");
 
     for (p = 1; p <= 5; p++) {
-        printf("Ingrese el nmero %d: ", p);
+        printf("Ingrese el numero %d: ", p);
         scanf("%f", &nur);
         sum += nur;
     }
